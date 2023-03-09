8b7deec6b713481952fbee2e816315648d9c5bc5
nefakemorfy <nefakemorfy@gmail.com>
2023-03-09 12:07:32 +0200

    zadacha 3

--- Program.cs
+++ Program.cs
@@ -10,26 +10,6 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            //int[] array = new int[6];
-            //for (int i = 0; i < 6; i++)
-            //{
-            //    Console.Write($"array[{i}]=");
-            //    array[i] = int.Parse(Console.ReadLine());
-            //}
-            //for (int i = 0; i < 6; i++)
-            //{
-            //    Console.WriteLine($"array[{i}]={array[i]}");
-            //}
-            //int[] array = new int[12];
-            //Random rnd = new Random();
-            //for (int i = 0; i < 12; i++)
-            //{
-            //    array[i] = rnd.Next(163, 190);
-            //}
-            //for (int i = 0; i < 6; i++)
-            //{
-            //    Console.WriteLine($"array[{i}]={array[i]}");
-            //}
             Console.Write("n=");
             byte n = byte.Parse(Console.ReadLine());
             double [] array = new double[n];
